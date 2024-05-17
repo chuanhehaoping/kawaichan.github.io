ead5b6264f900d1f615b2fa1fcd3d06c6dd50a39
chuanhehaoping <tenkawai1985@gmail.com>
2024-05-18 07:54:21 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var circle = document.getElementById("circle");
+var upBtn = document.getElementById("upBtn");
+var downBtn = document.getElementById("downBtn");
+
+var rotateValue = circle.style.transform;
+// console.log(rotateValue)
+var rotateSum;
+
+upBtn.onclick = function() {
+    /* 時計周りに90度回転させる*/
+    rotateSum = rotateValue + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateValue
+};
+downBtn.onclick = function() {
+    /* 半時計周りに90度回転させる*/
+    rotateSum = rotateValue + "rotate(90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateValue
+};
